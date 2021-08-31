96e8ce42ed8bb2890c1c5f58051ecc571b04e5a5
Ivan <73944911+Gennod@users.noreply.github.com>
2021-08-31 17:04:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('.carousel__slider').slick({
         autoplay: true,
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/right.png"></button>'
     });
 });
 
